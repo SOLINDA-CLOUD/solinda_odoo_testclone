a9bf172d58325189238be50d41b87d9da60d8209
inicoding <irfannurrahmat@outlook.com>
2022-06-29 13:08:20 +0700

    updt PR  line

---
+++ solinda_purchase/models/purchase_request.py
@@ -0,0 +1,10 @@
+from odoo import models, fields, api
+
+class PurchaseRequest(models.Model):
+    _inherit = 'purchase.request'
+
+class PurchaseRequestLine(models.Model):
+    _inherit = 'purchase.request.line'
+
+    project_code = fields.Char('Project Code')
+    budget_code = fields.Char('Budget Code')
